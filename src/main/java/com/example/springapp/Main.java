b4ec1717329e5247887b2510cdfbd6a46888e022
Ilya <svaikinilya@yandex.ru>
2022-05-16 04:48:21 +0300

    jwt

--- src/main/java/com/example/springapp/Main.java
+++ src/main/java/com/example/springapp/Main.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
+// пробую git с jwt
+
 public class Main
 {
     public static Connection getNewConnection(String dbURL, String username, String password) throws SQLException
